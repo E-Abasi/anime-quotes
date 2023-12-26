319c18bfde8c8871dc5976c59a32933c0c4fb7e5
E-Abasi <sommie1201@gmail.com>
2023-12-25 21:47:27 -0500

    Creates and imports button into page

---
+++ frontend/src/components/Button.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<button class="btn btn-outline btn-primary">Generate</button>
+
+<style>
+
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+    import Button from "../components/Button.svelte";
+
 
 </script>
 
 <h1>Anime Quote machine</h1>
+<Button />
 
 <style>
-    
+
 </style>
